1de5547d4c6b296fbff3a7705637c2a00d90faef
mysper <anthony.a3203@gmail.com>
2020-11-30 16:31:13 +0800

    revise About page for device add link on contact

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -12,15 +12,15 @@ function About() {
                     <li>CHSH</li>
                     <li>NPTU</li>
                 </ul>
-                <div class="contact-me">
-                    <h3>Contact Me</h3>
-                    <span>Gmail:&#9;&#9;&#9;&#9;&#9;anthony.a3203@gmail.com</span>
-                    <span>Telegram:&#9;&#9;@Mirumodapon</span>
-                    <span>Twitter:&#9;&#9;&#9;@storyOFmysper</span>
-                    <span>Instagram:&#9;@mirumo_0915</span>
-                </div>
-                <img src="https://www.gravatar.com/avatar/b73e70db41cabc52e25aa9ade99500a1?s=250" alt="my-gravatar" />
             </div>
+            <div class="contact-me">
+                <h3>Contact Me</h3>
+                <span>Gmail:&#9;&#9;&#9;&#9;&#9;anthony.a3203@gmail.com</span>
+                <a href="https://t.me/@Mirumodapon">Telegram:&#9;&#9;@Mirumodapon</a>
+                <a href="https://twitter.com/storyOFmysper">Twitter:&#9;&#9;&#9;@storyOFmysper</a>
+                <a href="https://www.instagram.com/mirumo_0915/">Instagram:&#9;@mirumo_0915</a>
+            </div>
+            <img src="https://www.gravatar.com/avatar/b73e70db41cabc52e25aa9ade99500a1?s=350" alt="my-gravatar" />
         </main>
     );
 }
