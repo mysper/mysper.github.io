bd07968cd3edd5bccb1f48bdcc52a6d01f309471
mysper <anthony.a3203@gmail.com>
2021-01-02 18:49:01 +0800

    test api on gh pages

--- src/component/Post.jsx
+++ src/component/Post.jsx
@@ -1,10 +1,29 @@
-function Post() {
-    return (
-        <div style={{ position: 'absolute', top: 0, bottom: 0 }}>
-            <h1>Post Page</h1>
-            <h2>Coming Soon</h2>
-        </div>
-    )
+import React, { Component } from 'react';
+import axios from 'axios';
+class Post extends Component {
+    constructor() {
+        super();
+        this.state = { response: null };
+        axios.get('https://mysper-gh-pages.herokuapp.com/post/get/list').then(
+            response => {
+                this.setState({ response: response.data });
+                console.log(this.state);
+            }
+        );
+    }
+    render() {
+        return (
+            <div style={{ position: 'absolute', top: 0, bottom: 0 }}>
+                <h1>Post Page</h1>
+                <h2>Coming Soon</h2>
+                <h2>building api</h2>
+                {(this.state.response) ?
+                    this.state.response.map((post, index) =>
+                        <p style={{ fontSize: '24px' }}>{post.title}<sub style={{ fontSize: '12px' }}>{post.data}</sub></p >
+                    ) : 'null'
+                }
+            </div>
+        );
+    }
 }
-
 export default Post;
