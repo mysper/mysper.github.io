c396e0becdf25c1ace3764132b83829eb67501d0
mysper <anthony.a3203@gmail.com>
2021-01-06 00:40:53 +0800

    post list layout

--- src/component/Post/PostsList.jsx
+++ src/component/Post/PostsList.jsx
@@ -5,14 +5,19 @@ import { Link } from 'react-router-dom';
 class PostList extends Component {
     listRender = (list) => {
         return Object.keys(list).map(
-            index =>
-                <Link to={`/post/${list[index]._id}`} className="list_item" key={list[index]._id}>
-                    <span className="title">{list[index].title}</span>
-                    <div className="comment_data">
-                        <span className="comment">{list[index].commentSize} {`comment${(list[index].commentSize > 1) ? 's' : ''}`},</span>
-                        <span className="data">{list[index].data}</span>
-                    </div>
-                </Link>
+            index => {
+                let D = new Date(list[index].date);
+                console.log(D.getMonth());
+                return (
+                    <Link to={`/post/${list[index]._id}`} className="list_item" key={list[index]._id}>
+                        <span className="title">{list[index].title}</span>
+                        <div className="comment_date">
+                            <span className="comment">{list[index].commentSize} {`comment${(list[index].commentSize > 1) ? 's' : ''}`}</span>
+                            <span className="date">{`${D.getFullYear()}/${D.getMonth() + 1}/${D.getDate()}`}</span>
+                        </div>
+                    </Link>
+                );
+            }
         );
     }
     render() {
